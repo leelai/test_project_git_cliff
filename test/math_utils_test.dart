d9405975bf75613d3a9c93948e57f39779f4ced7
leelai <leelai.tw@gmail.com>
2026-02-02 16:21:16 +0800

    test: ✅ add unit test for sum function

---
+++ test/math_utils_test.dart
@@ -0,0 +1,8 @@
+import 'package:test/test.dart';
+import 'package:flutter_test_project/core/utils/math_utils.dart';
+
+void main() {
+  test('sum adds two numbers', () {
+    expect(sum(2, 3), 5);
+  });
+}
